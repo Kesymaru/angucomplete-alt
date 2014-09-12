f598a5535482603d0c3cbe67a573fb75c0ee0f19
Andrey Alfaro Alvarado <andrey.alfaro@bvmedia.cr>
2014-09-12 17:19:33 -0600

    added searchStr

--- angucomplete-alt.js
+++ angucomplete-alt.js
@@ -55,28 +55,10 @@ angular.module('angucomplete-alt', [] ).directive('angucompleteAlt', ['$q', '$pa
       fieldRequired: '@',
       fieldRequiredClass: '@',
       inputChanged: '=',
-      autoMatch: '@'
+      autoMatch: '@',
+	  searchStr: '=',
     },
     templateUrl: function(tElement, attr){
-
-		/*var defaultTemplate = '<div class="angucomplete-holder">' +
-			'  <input id="{{id}}_value" ng-model="searchStr" type="text" placeholder="{{placeholder}}" class="{{inputClass}}" ng-focus="resetHideResults()" ng-blur="hideResults()" autocapitalize="off" autocorrect="off" autocomplete="off" ng-change="inputChangeHandler(searchStr)"/>' +
-			'  <div id="{{id}}_dropdown" class="angucomplete-dropdown" ng-if="showDropdown">' +
-			'    <div class="angucomplete-searching" ng-show="searching" ng-bind="textSearching"></div>' +
-			'    <div class="angucomplete-searching" ng-show="!searching && (!results || results.length == 0)" ng-bind="textNoResults"></div>' +
-			'    <div class="angucomplete-row" ng-repeat="result in results" ng-click="selectResult(result)" ng-mouseover="hoverRow($index)" ng-class="{\'angucomplete-selected-row\': $index == currentIndex}">' +
-			'      <div ng-if="imageField" class="angucomplete-image-holder">' +
-			'        <img ng-if="result.image && result.image != \'\'" ng-src="{{result.image}}" class="angucomplete-image"/>' +
-			'        <div ng-if="!result.image && result.image != \'\'" class="angucomplete-image-default"></div>' +
-			'      </div>' +
-			'      <div class="angucomplete-title" ng-if="matchClass" ng-bind-html="result.title"></div>' +
-			'      <div class="angucomplete-title" ng-if="!matchClass">{{ result.title }}</div>' +
-			'      <div ng-if="matchClass && result.description && result.description != \'\'" class="angucomplete-description" ng-bind-html="result.description"></div>' +
-			'      <div ng-if="!matchClass && result.description && result.description != \'\'" class="angucomplete-description">{{result.description}}</div>' +
-			'    </div>' +
-			'  </div>' +
-			'</div>';*/
-
 		var defaultTemplate = 'views/angucomplete-alt.html';
 		return attr.template ? attr.template : defaultTemplate;
 	},
